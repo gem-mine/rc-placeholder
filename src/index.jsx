25856396553d6a22a81b18f8c60c2ce6dd1c4921
彭星(666595) <666595@nd.com.cn>
2018-12-18 19:11:24 +0800

    PropTypes兼容React高版本

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import * as placeholders from './placeholders'
 
 export default class ReactPlaceholder extends React.Component {
--- src/placeholders/TextBlock.js
+++ src/placeholders/TextBlock.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import TextRow from './TextRow'
 
 export default class TextBlock extends React.Component {
